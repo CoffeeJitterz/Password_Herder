cd1f9136c944edb5665830ede038e328f25868c6
Smiti <mishra.smiti06@gmail.com>
2021-10-18 17:59:43 -0400

    generate password, copy password

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,40 +1,57 @@
 // Client facing scripts here
-$(() => {
-  $("#copy").click(() => {
-   const copyPassword = $("copy_password");
-    copyPassword.select();
-     /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyPassword.value);
-    /* Alert the copied text */
-    alert("Copied the text: " + copyPassword.value);
-  })
-});
-
-function generatePassword(){
+// $(() => {
+//   $("#copy").click(() => {
+//    const copyPassword = $("#copy_password");
+//     copyPassword.select();
+//      /* Copy the text inside the text field */
+//     navigator.clipboard.writeText(copyPassword.value);
+//     /* Alert the copied text */
+//     alert("Copied the text: " + copyPassword.value);
+//   })
+// });
+function generatePassword() {
   let password = "";
+  let characters = "";
+  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  let number = "0123456789";
+  let symbol = "!@#$%&*%";
+  let isUpperCase = document.getElementById('upperCase').checked;
+  let isLowerCase = document.getElementById('lowerCase').checked;
+  let isNumber = document.getElementById('number').checked;
+  let isSymbol = document.getElementById('symbols').checked;
+  let passwordLength = document.getElementById('pwdlength').value;
 
-    let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    let number = "0123456789";
-    let symbol = "!@#$%&* %";
-    let length = "12";
-    let characters = upperCase + lowerCase + number + symbol;
-  for (let i = 0; i < length; i++){
+  if (isUpperCase) {
+    characters = characters + upperCase;
+  }
+  if (isLowerCase) {
+    characters = characters + lowerCase;
+  }
+  if (isNumber) {
+    characters = characters + number;
+  }
+  if (isSymbol) {
+    characters = characters + symbol;
+  }
+  for (let i = 0; i < passwordLength; i++) {
     //generatePassword
     createPassword = characters.charAt(Math.floor(Math.random() * characters.length));
-    password += createPassword ;
+    password += createPassword;
   }
-  document.getElementById('#pass-text').value = password;
+  document.getElementById('pass-text').value = password;
+  if (!(isUpperCase || isLowerCase || isNumber || isSymbol)) {
 
+    $(".empty").hide();
+    return;
+  }
 };
-
-
 
 // Copy password button
 function copy() {
   // get password from input text field
-  var copyText = document.getElementById("#pass-text");
+  let copyText = document.getElementById('pass-text').value;
+  console.log("copyText:", copyText);
   //Copy the text from the text field
-  navigator.clipboard.writeText(copyText.value);
+  navigator.clipboard.writeText(copyText);
 };
-
