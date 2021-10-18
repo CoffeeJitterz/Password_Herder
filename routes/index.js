525b8394e2810951cb026347b57f84218d894113
Josh Millard <josh.millard10@gmail.com>
2021-10-17 20:08:03 -0400

    basic password table created and connected to database

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,57 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+  // router.get("/", (req, res) => {
+  //     res.render ("index");
+  //   });
   router.get("/", (req, res) => {
-    console.log("this is getting");
-    res.render("index");
+    db.query(`SELECT websites.name, website_username, website_password
+    FROM passwords
+    JOIN websites ON websites.id = website_id;`)
+    .then(data => {
+      const passwords = data.rows;
+      console.log(passwords);
+      const templateVars = {passwords};
+      res.render("index", templateVars);
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .json({ error: err.message });
+    });
   });
 
 
 
   return router;
 };
+
+// //create password
+// const createPassword = (passwords) => {
+//   //const $password_container = $('#password_container');
+//    $password_container.empty();
+
+//   for(const password of passwords) {
+//     //serperate inputs
+//     const website = password.name;
+//     const username = password.website_username
+//     const password = password.website_password
+//     //create HTML insery
+//     const singlePassword = `
+//     <tbody>
+//     <tr>
+//       <th scope="row">1</th>
+//       <td>${website}</td>
+//       <td>${username}</td>
+//       <td>${password}</td>
+//       <td><button>COPY</button></td>
+//       <td><button>EDIT</button></td>
+//       <td><button>DELETE</button></td>
+//     </tr>
+//   </tbody>
+// </table>
+//     `
+//     $password_container.prepend(singlePassword);
+
+//   };
+// };
