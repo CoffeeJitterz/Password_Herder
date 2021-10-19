6307f1da9e2178832c420788c15ddce51a4d1e18
Smiti <mishra.smiti06@gmail.com>
2021-10-19 14:02:37 -0400

    Co-authored-by: YBYuan007 <YBYuan007@users.noreply.github.com> Co-authored-by: Josh Millard <CoffeeJitterz@users.noreply.github.com>

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,15 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-
   router.get("/", (req, res) => {
-    //console.log(req.body)
-    db.query(`SELECT passwords.id, websites.name, website_username, website_password
+    db.query(`SELECT websites.name, website_username, website_password
               FROM passwords
               JOIN websites ON websites.id = website_id;
               `)
     .then(data => {
+      //console.log(data)
       const passwords = data.rows;
-      const templateVars = {passwords, email: req.session.email};
+      const templateVars = {passwords};
       res.render("index", templateVars);
     })
     .catch(err => {
@@ -21,37 +20,9 @@ module.exports = (db) => {
     });
   });
 
-  router.get("/:id/copy", (req, res) => {
-   console.log("I AM COPY");
-   const passwordID = req.params.id;
-   console.log(passwordID);
-   db.query(` SELECT website_password
-              FROM passwords
-              WHERE id = ${passwordID}
-   `)
-   .then(password => {
-     console.log("I'm from THEN!");
-     const copiedPassword = password.rows[0].website_password;
-     console.log(copiedPassword);
-     res.redirect("/");
-   })
+  router.post("/", (req, res) => {
+    //console.log(req.body)
   });
-
-  router.post("/:id/delete", (req, res) => {
-    console.log("I AM DELETE")
-    const passwordID = req.params.id;
-   console.log(passwordID);
-   db.query(` DELETE FROM passwords
-              WHERE id = ${passwordID}
-   `)
-   .then(password => {
-     res.redirect("/");
-   })
-  })
-
-  router.post("/:id/edit", (req, res) => {
-    console.log("I AM EDIT")
-  })
 
 
   return router;
--- routes/register.js
+++ routes/register.js
@@ -21,34 +21,26 @@ module.exports = (db) => {
 
     console.log("add one user");
 
-    const salt = bcrypt.genSaltSync(10);
-
-    const hashPW = function(userPW) {
-      return bcrypt.hashSync(userPW, salt);
-    };
-
-    const conditions = [
-      // check if the organization already exist.
-      pool.query(`SELECT * FROM organizations WHERE name = $1;`, [org]),
-      // check if the email already exist.
-      pool.query(`SELECT * FROM users WHERE email = $1;`, [em])
-    ];
-
-    return Promise.all(conditions)
-    .then((results) =>{
-      if(results[0].rows.length === 1  && results[1].rows.length === 0) {
-        return pool
-        // insert the new user information.
-        .query (`INSERT INTO users (email, user_password) VALUES ($1, $2) RETURNING *`, [em, hashPW(pw)])
-        .then( (user) => {
-          console.log("this is user: ", user)
-          if(!user) {
-            console.log('error!');
-            return;
-          }
-          return user;
-        })
+    return pool
+    .query(`SELECT * from organizations where name = $1;`, [org])
+    .then((result) => {
+      console.log(result);
+      if (result.rows.length === 0) {
+        console.log("wrong org!");
+        return;
       }
+      return pool
+      .query (`INSERT INTO users (email, user_password) VALUES ($2, $3) RETURNING *`, [em, bcrypt.hashSync(pw)])
+      .then( (user) => {
+        if(!user) {
+          console.log('error!');
+          return;
+        }
+        return user ;
+      })
+    })
+    .catch((err) => {
+      return "line 44 error!";
     })
   }
 
@@ -56,8 +48,8 @@ module.exports = (db) => {
 
   router.post("/", (req, res) => {
     const info = req.body;
-    // console.log("This is posting");
-    // console.log("req.body: ", req.body);
+    console.log("This is posting");
+    console.log("req.body: ", req.body);
 
     addUser(info.organization_name, info.email, info.password)
     .then((result) => {
