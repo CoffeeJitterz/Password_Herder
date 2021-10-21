224a7e827643f7df4505b010335f82a84a25025a
YBYuan007 <yuanyanbin2011@gmail.com>
2021-10-21 20:13:44 +0000

    fix the seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,20 +3,20 @@
 -- INSERT INTO users (name) VALUES ('Kira');
 
 --users
-INSERT INTO users (id, email, user_password) VALUES (1, 'one@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (id, email, user_password) VALUES (2, 'two@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (id, email, user_password) VALUES (3, 'three@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (id, email, user_password) VALUES (4, 'four@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (email, user_password) VALUES ('one@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (email, user_password) VALUES ('two@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (email, user_password) VALUES ('three@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (email, user_password) VALUES ('four@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
-INSERT INTO users (id, email, user_password) VALUES (5, 'five@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (id, email, user_password) VALUES (6, 'six@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (id, email, user_password) VALUES (7, 'seven@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (id, email, user_password) VALUES (8, 'eight@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (email, user_password) VALUES ('five@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (email, user_password) VALUES ('six@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (email, user_password) VALUES ('seven@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (email, user_password) VALUES ('eight@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 
 -- organizations
-INSERT INTO organizations (id, name) VALUES (1,'cool');
-INSERT INTO organizations (id, name) VALUES (2,'verycool');
+INSERT INTO organizations (name) VALUES ('cool');
+INSERT INTO organizations (name) VALUES ('verycool');
 
 -- users_organizations:
 INSERT INTO users_organizations (user_id, organization_id) VALUES (1,1);
@@ -34,68 +34,68 @@ INSERT INTO users_organizations (user_id, organization_id) VALUES (8,2);
 
 
 
-INSERT INTO websites(id, name, category)
-VALUES (1, 'facebook', 'social');
+INSERT INTO websites(name, category)
+VALUES ('facebook', 'social');
 
-INSERT INTO websites(id, name, category)
-VALUES (2, 'twitter','social');
+INSERT INTO websites(name, category)
+VALUES ('twitter','social');
 
-INSERT INTO websites(id, name, category)
-VALUES (3, 'instagram','social');
+INSERT INTO websites(name, category)
+VALUES ('instagram','social');
 
-INSERT INTO websites(id, name, category)
-VALUES (4, 'linkedin','job');
+INSERT INTO websites(name, category)
+VALUES ('linkedin','job');
 
-INSERT INTO websites(id, name, category)
-VALUES (5, 'bamboo','job');
+INSERT INTO websites(name, category)
+VALUES ('bamboo','job');
 
-INSERT INTO websites(id, name, category)
-VALUES (6, 'netflix', 'entertainment');
+INSERT INTO websites(name, category)
+VALUES ('netflix', 'entertainment');
 
-INSERT INTO websites(id, name, category)
-VALUES (7, 'crave','entertainment');
+INSERT INTO websites(name, category)
+VALUES ('crave','entertainment');
 
-INSERT INTO websites(id, name, category)
-VALUES (8, 'lighthouse','educational');
+INSERT INTO websites(name, category)
+VALUES ('lighthouse','educational');
 
-INSERT INTO websites(id, name, category)
-VALUES (9, 'alsolighthouse','educational');
+INSERT INTO websites(name, category)
+VALUES ('alsolighthouse','educational');
 
-INSERT INTO websites(id, name, category)
-VALUES (10, 'eventbrite','hobby');
+INSERT INTO websites(name, category)
+VALUES ('eventbrite','hobby');
 
 
 
 -- passwords
 
-INSERT INTO passwords (id, organization_id, user_id, website_id, website_username, website_password)
-VALUES (1,1,1,1, 'fb', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
+INSERT INTO passwords (organization_id, user_id, website_id, website_username, website_password)
+VALUES (1,1,1, 'fb', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
 
-INSERT INTO passwords (id, organization_id, user_id, website_id, website_username, website_password)
-VALUES (2,1,1,2, 'tw', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
+INSERT INTO passwords (organization_id, user_id, website_id, website_username, website_password)
+VALUES (1,1,2, 'tw', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
 
-INSERT INTO passwords (id, organization_id, user_id, website_id, website_username, website_password)
-VALUES (3,1,1,3, 'ins', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
+INSERT INTO passwords (organization_id, user_id, website_id, website_username, website_password)
+VALUES (1,1,3, 'ins', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
 
-INSERT INTO passwords (id, organization_id, user_id, website_id, website_username, website_password)
-VALUES (4,1,2,4, 'li', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
+INSERT INTO passwords (organization_id, user_id, website_id, website_username, website_password)
+VALUES (1,2,4, 'li', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
 
-INSERT INTO passwords (id, organization_id, user_id, website_id, website_username, website_password)
-VALUES (5,1,2,5, 'bb', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
+INSERT INTO passwords (organization_id, user_id, website_id, website_username, website_password)
+VALUES (1,2,5, 'bb', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
 
 
-INSERT INTO passwords (id, organization_id, user_id, website_id, website_username, website_password)
-VALUES (6,2,5,6, 'nf', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
+INSERT INTO passwords (organization_id, user_id, website_id, website_username, website_password)
+VALUES (2,5,6, 'nf', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
 
-INSERT INTO passwords (id, organization_id, user_id, website_id, website_username, website_password)
-VALUES (7,2,5,7, 'cr', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
+INSERT INTO passwords (organization_id, user_id, website_id, website_username, website_password)
+VALUES (2,5,7, 'cr', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
 
-INSERT INTO passwords (id, organization_id, user_id, website_id, website_username, website_password)
-VALUES (8,2,6,8, 'lh', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
+INSERT INTO passwords (organization_id, user_id, website_id, website_username, website_password)
+VALUES (2,6,8, 'lh', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
 
-INSERT INTO passwords (id, organization_id, user_id, website_id, website_username, website_password)
-VALUES (9,2,6,9, 'slh', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
+INSERT INTO passwords (organization_id, user_id, website_id, website_username, website_password)
+VALUES (2,6,9, 'slh', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
 
-INSERT INTO passwords (id, organization_id, user_id, website_id, website_username, website_password)
-VALUES (10,2,6,10, 'eb', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
+INSERT INTO passwords (organization_id, user_id, website_id, website_username, website_password)
+VALUES (2,6,10, 'eb', 'U2FsdGVkX1/ipuRR1jjBuF2MJy7F+8R+6Neg+41dM7U=');
 
